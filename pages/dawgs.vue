fe4257d53f22ca9d645bb123e2be75740d64d5bc
meow <ashley-293@hotmail.com>
2024-01-22 13:16:31 -0700

    ape

---
+++ pages/dawgs.vue
@@ -0,0 +1,21 @@
+<script setup>
+// Fetch data from the dogs API endpoint
+const { data } = await useFetch("/api/dawgs");
+// clean up the data
+const { _value: dataValues } = data;
+const { message: breeds } = dataValues;
+</script>
+<template>
+  <main>
+    <header>
+      <h1>Dog Breeds</h1>
+    </header>
+    <div>
+      <ul>
+        <li v-for="breed in breeds" :key="breed.id">
+          {{ breed }}
+        </li>
+      </ul>
+    </div>
+  </main>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+
+<template>
+
+  <main>
+  
+  <h1> Welcome</h1>
+  
+  <p>Click on the top right where it says "Dawgs"</p>
+  
+  </main>
+  
+  
+  </template>

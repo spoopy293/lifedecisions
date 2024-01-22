b936e63f49335ccf8cfd388b2032651611361361
meow <ashley-293@hotmail.com>
2024-01-22 13:24:28 -0700

    fix

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
-<template>
-  <div>
-    <NuxtWelcome />
+<template>//
+  <div id="app">
+    <AppHeader />
+    <NuxtPage />
   </div>
 </template>
+
+<style>
+body,
+.app {
+  margin: 0;
+}
+</style>
